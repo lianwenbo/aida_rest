5a70c4e82c5d363d60e34aa50bc142c7f14ee3a5
lianwenbo <lianwenbo@douban.com>
2017-12-26 09:56:22 +0800

    fix: setting for the mysql environ& add pymysql
    1)在测试mysql的环境时，必须使用pymysql，否则运行不起来
    2)修改了models中的一些column的配置

--- setup_flask_env.sh
+++ setup_flask_env.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-export ACCESS_KEY_ID=''
-export ACCESS_KEY_SECRET=''
+#export ACCESS_KEY_ID=''
+#export ACCESS_KEY_SECRET=''
 #export SECRET_KEY=''
-#DATABASE_URL=''
-#DEV_DATABASE_URL=''
-#TEST_DATABASE_URL=''
+#export DATABASE_URL='mysql+pymysql://user:password@hostname/database'
+#export FLASK_CONFIG='production'
+#export DEV_DATABASE_URL=''
+#export TEST_DATABASE_URL=''
